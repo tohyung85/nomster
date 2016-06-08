4a5270e913397aed50e2287f4389d548e3f94f42
Your Name <tantohyung@gmail.com>
2016-06-08 14:38:58 +0000

    Include validation of place name

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,12 +7,16 @@ class PlacesController < ApplicationController
   end
 
   def new    
-    @places = Place.new
+    @place = Place.new
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -32,7 +36,11 @@ class PlacesController < ApplicationController
       return render text: 'Not Allowed', status: :forbidden
     end
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
